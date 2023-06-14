55c5debd54bcbb6254d7d382341f68874b64392e
domenico2003 <domdat03@gmail.com>
2023-06-14 16:08:18 +0200

    commit da terminale

---
+++ src/test/java/Aplication/U5S3G3DattiloDomenicoApplicationTests.java
@@ -0,0 +1,13 @@
+package Aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S3G3DattiloDomenicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
